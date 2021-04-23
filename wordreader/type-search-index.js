7de55e38e1b27cacd88590b7c947dbf0b32833e5
lukasbeckercode <lukas.becker.lb@outlook.com>
2021-04-23 23:39:13 +0200

    added javadoc for wordReader

---
+++ wordreader/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.lukabeckercode.wordreader","l":"NumberComparator"},{"p":"com.lukabeckercode.wordreader","l":"WordReader"}];updateSearchResults();
